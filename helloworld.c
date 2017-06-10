31a8162a6a631575d17651a031be9d644da8dcd0
neels <neelssleen30@gmail.com>
2017-06-10 17:45:25 +0530

    initial commit

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+
+//Sample hello world program
+
+#include<stdio.h>  //Standard input and output printf, scanf 
+#include<stdlib.h> //Standard library 
+int main(){
+   int number; //Datatype int,char,array,float
+   char str[20];		
+   printf("Enter your name:"); // Print data	 
+    scanf("%s",  str);	
+   printf("hi %s",str);	
+   return 0; // return statements
+}
+
